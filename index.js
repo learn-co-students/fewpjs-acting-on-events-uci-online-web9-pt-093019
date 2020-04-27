af7142accdeda9abd7f95ed5d5d17deeb7596942
ACAPP-dev <acapp909@gmail.com>
2020-04-27 16:43:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Your code here
+
+let dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = "#FF69B4";
+
+
+document.addEventListener('keydown', logEvent);
+
+function logEvent(e) {
+    switch (e.key) {
+        case "ArrowLeft": moveDodgerLeft()
+            break;
+        case "ArrowRight": moveDodgerRight()
+            break;
+        case "ArrowUp": moveDodgerUp()
+            break;
+        case "ArrowDown": moveDodgerDown()
+            break;
+    }
+}
+
+function moveDodgerLeft() {
+    const currLeft = parseInt(dodger.style.left);
+    if (currLeft >= 10) {
+        dodger.style.left = `${currLeft - 10}px`;
+    }
+    else {
+        dodger.style.left = "0px";
+    }
+}
+
+function moveDodgerRight() {
+    const currLeft = parseInt(dodger.style.left);
+    if (currLeft < 360) {
+        dodger.style.left = `${currLeft + 10}px`;
+    }
+    else {
+        dodger.style.left = "360px";
+    }
+}
+
+function moveDodgerUp() {
+    const currBottom = parseInt(dodger.style.bottom);
+    if (currBottom <= 370) {
+        dodger.style.bottom = `${currBottom + 10}px`;
+    }
+    else {
+        dodger.style.Bottom = "370px";
+    }
+}
+
+function moveDodgerDown() {
+    const currBottom = parseInt(dodger.style.bottom);
+    if (currBottom >= 10) {
+        dodger.style.bottom = `${currBottom - 10}px`;
+    }
+    else {
+        dodger.style.Bottom = "0px";
+    }
+}
